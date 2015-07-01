56ac39f8b4e504bd1f0b0584d19beae79b1b9646
Ben Cutrell <bcutrell@dhcp-a-092.int.smartleaf.com>
2015-07-01 09:57:40 -0400

    adds skeleton

--- src/js/components/TickerBox.jsx
+++ src/js/components/TickerBox.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Task from './Task.jsx';
 import ListGroup from 'react-bootstrap/lib/ListGroup';
 import Alert from 'react-bootstrap/lib/Alert';
 
@@ -21,15 +20,14 @@ var TickerForm = React.createClass({
   
   render: function() {
     return (
-
-<div className="row">
-  <div className="col-lg-12">
-    <form id="tickerForm" className="topBefore">
-      <input id="tickerInput" type="text" placeholder="TICKER" ref="myTickerInput" />
-      <input id="submit" type="submit" value="GO!" onClick={this.handleClick} />
-    </form>
+      <div class="row">
+      <div className="col-centered">
+      <form id="tickerForm" className="topBefore">
+        <input id="tickerInput" type="text" placeholder="TICKER" ref="myTickerInput" />
+        <input id="submit" type="submit" value="GO!" onClick={this.handleClick} />
+      </form>
+    </div>
   </div>
-</div>
     );
   }
 });
@@ -48,19 +46,14 @@ var TickerInfoRow = React.createClass({
     var displayInfoCol = this.infoTypes().map(function (infoCol) {
 
   return (
-<div className="col-lg-2">
-  <div className="alert alert-success min-alert-height" role="alert">
-    <div className="text-center"><strong>{infoCol.Name}</strong></div>
-    <div id={infoCol.Id} className="text-center"></div>
-  </div>
-</div>
+    <li className="text-center">{infoCol.Name}</li>
     );
   });
 
   return (
-<div className="row">
+<ul className="row">
   { displayInfoCol }
-</div>
+</ul>
     );
   }
 });
